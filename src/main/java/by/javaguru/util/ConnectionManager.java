1557e9edaf0f65a67882319d60b1fd3edf62c3c2
Evgeniy <paut.evg@yandex.ru>
2023-10-02 18:31:43 +0300

    feature(connection-manager): add logging

--- src/main/java/by/javaguru/util/ConnectionManager.java
+++ src/main/java/by/javaguru/util/ConnectionManager.java
@@ -1,10 +1,14 @@
 package by.javaguru.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
+    private static final Logger logger = LoggerFactory.getLogger(ConnectionManager.class);
     private static final String URL_KEY = "db.url";
     private static final String USERNAME_KEY = "db.username";
     private static final String PASSWORD_KEY = "db.password";
@@ -21,6 +25,7 @@ public class ConnectionManager {
                                 PropertiesUtil.get(URL_KEY),
                                 PropertiesUtil.get(USERNAME_KEY),
                                 PropertiesUtil.get(PASSWORD_KEY));
+                        logger.info("Connection opened. Schema: {}", connection.getSchema());
                     }
                 }
             }
@@ -33,6 +38,7 @@ public class ConnectionManager {
     public static void close() {
         try {
             connection.close();
+            logger.info("Connection closed");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
