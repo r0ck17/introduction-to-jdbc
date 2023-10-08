f97d00df89d06dc7d07cde523fd7f3d3e5424f16
Evgeniy <paut.evg@yandex.ru>
2023-10-08 14:18:23 +0300

    style(dao): remove comments

--- src/main/java/by/javaguru/dao/FlightDao.java
+++ src/main/java/by/javaguru/dao/FlightDao.java
@@ -1,9 +1,7 @@
 package by.javaguru.dao;
 
-import by.javaguru.dto.FlightUpdateInfo;
-import by.javaguru.dto.TicketUpdateInfo;
 import by.javaguru.entity.Flight;
 
 public interface FlightDao extends Dao<Long, Flight> {
-    boolean updateDataByFlightId(Long id, FlightUpdateInfo flightInfo, TicketUpdateInfo ticketInfo);
+
 }
--- src/main/java/by/javaguru/dao/TicketDao.java
+++ src/main/java/by/javaguru/dao/TicketDao.java
@@ -1,15 +1,7 @@
 package by.javaguru.dao;
 
-import by.javaguru.dto.TicketFilter;
-import by.javaguru.dto.TicketUpdateInfo;
 import by.javaguru.entity.Ticket;
 
-import java.util.List;
-import java.util.Map;
-
 public interface TicketDao extends Dao<Long, Ticket> {
-    int updateTickets(TicketFilter ticketFilter, TicketUpdateInfo updateInfo);
-    List<Ticket> findTicketsByFlightId(Long flightId);
-    List<String> findMostCommonNames(int limit);
-    Map<String, Integer> findPassengerTotalTicketCount();
+
 }
