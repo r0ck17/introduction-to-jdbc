680916b994717655426c8318ab3069196e38f9cf
Evgeniy <paut.evg@yandex.ru>
2023-10-08 14:20:07 +0300

    feature(entity): add embedded pk for seat

---
+++ src/main/java/by/javaguru/entity/SeatId.java
@@ -0,0 +1,19 @@
+package by.javaguru.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class SeatId implements Serializable {
+    private Integer aircraftId;
+    private String seatNo;
+}
